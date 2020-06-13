1b5774d12098d8ee775a624834f22ad2c85a2993
masterambi <ramzyrash@gmail.com>
2020-06-13 20:55:16 +0700

    Added tableview, custom protocol in the model, displaying video title in the tableview

--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -8,7 +8,14 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    func videosFetched(_ videos: [Video])
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
+    
     func getVideos() {
         
         // Create a URL object
@@ -35,7 +42,14 @@ class Model {
                 
                 let response = try decoder.decode(Response.self, from: data!)
                 
-                dump(response)
+                if response.items != nil {
+                    DispatchQueue.main.async {
+                        // Call the "videosFetched" method of the delegate
+                        self.delegate?.videosFetched(response.items!)
+                    }
+                }
+                
+                // dump(response)
             }
             catch {
                 
